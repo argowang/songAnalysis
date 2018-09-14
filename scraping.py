91e8d2e41c309a4e820af37ef40ee902d52ca504
argowang <wangjunyi@hotmail.com>
2018-09-13 20:14:30 -0400

    refactor scraping code

--- scraping.py
+++ scraping.py
@@ -7,61 +7,74 @@ import re
 from bs4 import BeautifulSoup
 
 
-singer_id_list = ['1008034', '1197115','12493701', '12127564', '1204010', '1038099', '12021086', '784257', '1211046']
+SINGER_ID_LIST = ['1008034', '1197115','12493701', '12127564', '1204010', '1038099', '12021086', '784257', '1211046']
 
-for id in singer_id_list:
-	singer_url = 'http://music.163.com/artist?id=' + str(id)
-	web_data = requests.get(singer_url)
-	soup = BeautifulSoup(web_data.text, 'lxml')
-	singer_name = soup.select("#artist-name")
-	singer_name = singer_name[0].get('title')
-	r = soup.find('ul', {'class': 'f-hide'}).find_all('a')
-	r = (list(r))
-	music_name_set=[]
-	music_id_set=[]
+ARTIST_ENDPOINT = 'http://music.163.com/#/artist?id='
+LYRICS_ENDPOINT = 'http://music.163.com/api/song/lyric?id=%s&lv=1&kv=1&tv=-1'
+SONG_LIST_OUTPUT_DIR = '/home/mrdoggie/songAnalysis/data/scrappedSongList.txt'
+LYRICS_OUTPUT_DIR = '/home/mrdoggie/songAnalysis/data/scrappedLyrics.txt'
 
+def processSingleSinger(singerID):
+    music_name_set, music_id_set = fetchSongIDListGivenSingleSingerID(singerID)
+    for musicName in music_name_set:
+        processSingleMusicName(musicName)
+    for musicID in music_id_set:
+        recordLyricsGivenSingleMusicID(musicID)
 
-	# get the list of song id from a webpage
-	for each in r:
-		song_name = each.text
-		music_name_set.append(song_name)
-		# print(each.text)
-		song_id = each.attrs["href"]
-		music_id_set.append(song_id[9:])
+def processSingleMusicName(musicName):
+    for each in music_name_set:
+        f = open(SONG_LIST_OUTPUT_DIR,"ab")
+        try:
+            f.write(singer_name.encode('utf-8') + each.encode('utf-8')  + '\n') #one song name each line
+            f.close
+        except AttributeError as e2:
+            pass
 
-	# record the name of the songs analyzed
+def recordLyricsGivenSingleMusicID(musicID):
+    lrc_url = LYRICS_ENDPOINT % musicID
+    lyric = requests.get(lrc_url)
+    json_obj = lyric.text
+    j = json.loads(json_obj)
+    try: #some song may not have lyrics
+        lrc=j['lrc']['lyric']
+        pat=re.compile(r'\[.*\]')
+        lrc=re.sub(pat,"",lrc)
+        lrc=lrc.strip()
+    except KeyError as e:
+        pass
+        # Reformat the lyrics. Get ready for later transcription. Notice: this will mess up the looking in texteditor
+    all_lyrics = re.sub(r' ','\n',lrc) #convert all whitespace to newline to unify the format
+    all_lyrics = re.sub(r'\n+', '\n', all_lyrics) #remove duplicated newline
+    f = open(LYRICS_OUTPUT_DIR, "ab")
+    try:
+    	f.write(all_lyrics.encode('utf-8'))
+    	f.close
+    except AttributeError as e2:
+    	pass
 
-	for each in music_name_set:
-		# f = open("/Users/mrdoggie/Desktop/Project/hipHopAnalysis/scrappedSongList.txt", "ab") #on mac
-		f = open("C:/github/songAnalysis/scrappedSongList.txt", "ab") #on windows
-		try:
-			f.write(singer_name.encode('utf-8') + each.encode('utf-8')  + '\n') #one song name each line
-			f.close
-		except AttributeError as e2:
-			pass
+def fetchSongIDListGivenSingleSingerID(singerID):
+    singer_url = ARTIST_ENDPOINT + str(singerID)
+    web_data = requests.get(singer_url)
+    print(web_data.text)
+    soup = BeautifulSoup(web_data.text, 'lxml')
+    singer_name = soup.select("#artist-name")
+    singer_name = singer_name[0].get('title')
+    r = soup.find('ul', {'class': 'f-hide'}).find_all('a')
+    r = (list(r))
+    music_name_set=[]
+    music_id_set=[]
+    # get the list of song id from a webpage
+    for each in r:
+        song_name = each.text
+        music_name_set.append(song_name)
+        # print(each.text)
+        song_id = each.attrs["href"]
+        music_id_set.append(song_id[9:])
+    return music_name_set, music_id_set
 
-	# record the lyrics
-	for each in music_id_set:
-		lrc_url = 'http://music.163.com/api/song/lyric?' + 'id=' + str(each) + '&lv=1&kv=1&tv=-1'
-		lyric = requests.get(lrc_url)
-		json_obj = lyric.text
-		j = json.loads(json_obj)
-		try: #some song may not have lyrics
-			lrc=j['lrc']['lyric']
-			pat=re.compile(r'\[.*\]')
-			lrc=re.sub(pat,"",lrc)
-			lrc=lrc.strip()
-		except KeyError as e:
-			pass
-		# Reformat the lyrics. Get ready for later transcription. Notice: this will mess up the looking in texteditor
-		all_lyrics = re.sub(r' ','\n',lrc) #convert all whitespace to newline to unify the format
-		all_lyrics = re.sub(r'\n+', '\n', all_lyrics) #remove duplicated newline
-		
+def main():
+    for singerID in SINGER_ID_LIST:
+        processSingleSinger(singerID)
 
-		# f = open("/Users/mrdoggie/Desktop/Project/hipHopAnalysis/scrappedLyrics.txt", "ab") #on mac
-		f = open("C:/github/songAnalysis/scrappedLyrics.txt", "ab") #on windows
-		try:
-			f.write(all_lyrics.encode('utf-8'))
-			f.close
-		except AttributeError as e2:
-			pass
+if __name__ == "__main__":
+    main()
